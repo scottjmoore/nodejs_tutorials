1f6848e1989af8904e215677b1facf283b69dc4a
Scott Moore <scott.moore@scottmooresoftware.com>
2019-05-31 20:12:00 +0100

    Add Firebase SDK

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-console.log('Hello from Node.JS');
-
-const Person = require('./person');
-const person1 = new Person('John Doe', 31);
-
-console.log(person1.greeting());
+const firebaseConfig = {
+    apiKey: "AIzaSyBK1HAJDBjfabhJF0Id67YER_bXJRRcrXs",
+    authDomain: "testapp-7d530.firebaseapp.com",
+    databaseURL: "https://testapp-7d530.firebaseio.com",
+    projectId: "testapp-7d530",
+    storageBucket: "testapp-7d530.appspot.com",
+    messagingSenderId: "429130421915",
+    appId: "1:429130421915:web:3314ca66f9e1c79c"
+  };
