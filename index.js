f3194a17aabace33bbe9d3f5a283998587dd220b
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-05-31 20:26:56 +0100

    Clean up and start again

--- index.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBK1HAJDBjfabhJF0Id67YER_bXJRRcrXs",
-    authDomain: "testapp-7d530.firebaseapp.com",
-    databaseURL: "https://testapp-7d530.firebaseio.com",
-    projectId: "testapp-7d530",
-    storageBucket: "testapp-7d530.appspot.com",
-    messagingSenderId: "429130421915",
-    appId: "1:429130421915:web:3314ca66f9e1c79c"
-  };
